c4626a1b32b4a7dd78667a7c1ca62eae36353bf0
Colin Jaffe <colin.jaffe@gmail.com>
2023-08-29 16:32:59 -0400

    Removes solution from template repo.

--- interface.js
+++ interface.js
@@ -1,7 +1,1 @@
-const userInput = prompt(
-  "Enter a temperature in degrees Celsius or Fahrenheit"
-);
-
-const cleanInput = removeSpaces(userInput);
-const answer = convertTemp(cleanInput);
-console.log(`${cleanInput} converted is:\n\n${answer} degrees.`);
+// Add your interface code below!
