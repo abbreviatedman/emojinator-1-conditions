5ac570dc19d31475dc9f3b658f67d8eac26102cc
Colin Jaffe <colin.jaffe@gmail.com>
2023-08-29 17:56:31 -0400

    Adds full solution.

--- logic.js
+++ logic.js
@@ -1,5 +1,30 @@
 // Add your business logic functions below!
 
-function getEmoji() {
-  return '☺';
+function getEmoji(emojiDescription) {
+  if (emojiDescription === "smile") {
+    return "☺";
+  } else if (emojiDescription === "frown") {
+    return "☹";
+  } else if (emojiDescription === "heart") {
+    return "❤";
+  } else if (emojiDescription === "star") {
+    return "⭐";
+  } else {
+    return "Sorry, I don't know that emoji.";
+  }
+}
+
+function getEmojiBonusVersion(emojiDescription) {
+  const emojiList = {
+    smile: "☺",
+    frown: "☹",
+    heart: "❤",
+    star: "⭐",
+  };
+
+  if (emojiList[emojiDescription]) {
+    return emojiList[emojiDescription];
+  } else {
+    return "Sorry, I don't know that emoji.";
+  }
 }
