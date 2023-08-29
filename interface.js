4182da76cc9a99c165da6d69fd46df298a5aedd9
Colin Jaffe <colin.jaffe@gmail.com>
2023-08-29 18:08:16 -0400

    Adds basic interface.

--- interface.js
+++ interface.js
@@ -1,1 +1,11 @@
 // Add your interface code below!
+
+const input = prompt("Please enter a description of your emoji.");
+
+const emoji = getEmoji(input);
+
+if (emoji === "Sorry, I don't know the emoji.") {
+  console.log();
+} else {
+  console.log("Your emoji is: " + emoji);
+}
